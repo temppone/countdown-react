507ef515e74ba7a5f61944426fdb9ae58e44324b
gustavo <gtvtempone@gmail.com>
2021-04-27 17:50:18 -0300

    Criação do form com input para a data futura

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
+  const [date, setDate] = React.useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(date);
+  };
 
   //Função que calcula o tempo restante
   const calculateTimeLeft = () => {
@@ -9,7 +15,8 @@ const App = () => {
     let year = new Date().getFullYear();
 
     //O ano pode ser um input do usuário e também é possível adicionar outro inputs como data e mês ao new Date()
-    const diff = +new Date(`10/01/${year}`) - +new Date();
+    const diff = +new Date(`${date}`) - +new Date();
+    console.log(diff);
     let timeLeft = {};
 
     //Testa pra saber se o valor do tempo restante é maior que 0, caso seja, transforma todos os valores pra o padrão de data e hora
@@ -20,19 +27,13 @@ const App = () => {
         minutes: Math.floor((diff / 1000 / 60) % 60),
         seconds: Math.floor((diff / 1000) % 60),
       };
-      //Tentantiva de fazer um pouco mais inteligente
-      // timeLeft = {
-      //   seconds: Math.floor((diff / 1000) % 60),
-      //   minutes: Math.floor((this.seconds / 60) % 60),
-      //   hours: Math.floor((this.minutes / 60) % 60),
-      //   days: Math.floor((this.hours / 24) % 24),
-      // };
-      // console.log(timeLeft);
     }
     return timeLeft;
   };
+
   const [timeLeft, setTimeLeft] = React.useState(calculateTimeLeft());
 
+  //Altera o valor do time left a cada 1000ms
   React.useEffect(() => {
     const timer = setTimeout(() => {
       setTimeLeft(calculateTimeLeft());
@@ -56,7 +57,19 @@ const App = () => {
 
   return (
     <div>
-
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="date"></label>
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+          <button>Começar</button>
+        </form>
+      </div>
       <div>
         {timerComponents.length ? timerComponents : <span>Time's up!</span>}
       </div>
